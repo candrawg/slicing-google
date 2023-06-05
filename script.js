4a70ba63b9735d1868ec095f1ee1ebc346fc8762
candrawg <candra.wijaya.gulo15@gmail.com>
2023-06-05 16:47:49 +0700

    Added a light mode

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Light Mode
+if (localStorage.getItem('theme') == 'light') {
+	setLightMode();
+}
+
+function setLightMode() {
+	let icon = '';
+	let isLight = document.body.classList.toggle('light-mode');
+
+	if (isLight) {
+		icon = '<i class="material-icons ">brightness_high</i>';
+		localStorage.setItem('theme', 'light');
+	} else {
+		icon = '<i class="material-icons ">brightness_2</i>';
+		localStorage.removeItem('theme');
+	}
+	document.getElementById('lightBtn').innerHTML = icon;
+}
+
+// Search
 function search() {
 	let { value } = document.getElementById('searchInput');
 
@@ -6,4 +26,11 @@ function search() {
 		url = '#';
 	}
 	location.replace(url + value);
+}
+
+function handleKeyPress(event) {
+	if (event.keyCode === 13) {
+		// Panggil fungsi yang diinginkan di sini
+		search();
+	}
 }
